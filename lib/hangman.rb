2d74a0ca66332e4b481ee6d3d8a241ddb35062fd
coveredinclover <rphale3000@gmail.com>
2023-01-09 13:11:11 -0800

    Fix clean dict and add function for collecting guesses

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,6 @@
 class Hangman
   def self.clean_dict
-    @dictionary = File.readlines($contents).map &:split
+    @dictionary = File.readlines($contents, chomp: true)
     p @dictionary.length
     @rejected = @dictionary.reject {|word| word.length < 5 || word.length > 12}
     p @rejected.length
@@ -16,17 +16,32 @@ class Hangman
     p "You have #{@guesses} guesses left."
   end
 
-  def self.game_start
+  def self.get_guess
     p @picked_word
+    p "Please type in your guess."
+    @guess = gets.chomp.gsub(/\W/, "")
+    
+    if @guess == nil
+      fail_state
+    elsif @guess.lenth > 1
+      fail_state
+    end
   end
 
+  def fail_state
+
+  end
+
+  def self.check_guess
+    word_array = @picked_word.chars
+  end
 end
 
 
 
-$contents = File.open('/Users/clover/Documents/hangman/google-10000-english-no-swears.txt', 'r')
+$contents = File.open('/Users/clover/Documents/hangman1/google-10000-english-no-swears.txt', 'r')
 
 Hangman.clean_dict
 Hangman.pick_word(@dictionary)
 Hangman.print_guesses_left
-Hangman.game_start
+Hangman.check_guess
